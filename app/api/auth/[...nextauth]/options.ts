636e838c7dd85a7cdad37d3517b922f2f404bc97
ProdanFattan <prodanfattan@gmail.com>
2025-01-27 10:42:04 +0600

    next auth setup and sessionProvider done

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,73 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "Credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found");
+          }
+          if (!user.isVerified) {
+            throw new Error("User is not verified");
+          }
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isPasswordCorrect) {
+            throw new Error("Password is incorrect");
+          } else {
+            return user;
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+  },
+};
--- types/ApiResponse.ts
+++ types/ApiResponse.ts
@@ -1,7 +1,7 @@
-import { Message } from "@/model/User.model"
-;export interface ApiResponse {
-    success: boolean;
-    message: string;
-    isAcceptingMessages: boolean;
-    messages?: Array<Message>;
+import { Message } from "@/model/User.model";
+export interface ApiResponse {
+  success: boolean;
+  message: string;
+  isAcceptingMessages: boolean;
+  messages?: Array<Message>;
 }
